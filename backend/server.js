1ce5ec9996e5eb6ff110d9fa3d107ee50fd7e6e3
Tom Luxton <thomas.luxton@yahoo.com.au>
2022-09-11 14:47:52 +1000

    added get request for api. Welcome message

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('server...')
+const express = require('express')
+const dotenv = require ('dotenv').config()
+const PORT = process.env.PORT || 8000
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'welcome to the support desk API'})
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
