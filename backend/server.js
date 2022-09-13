eb5fb2d4a2c085f8fe360171a67ef16815b0dbf1
Tom Luxton <thomas.luxton@yahoo.com.au>
2022-09-13 11:42:41 +1000

    Added error db.js to connect database. Installed colors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require ('dotenv').config()
 const{errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
+
+//connect to db
+connectDB()
+
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
